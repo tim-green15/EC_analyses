403eef80c24f0f2494e2d54e7c6d6f93ecfb4718
tim-green15 <tim.green@ed.ac.uk>
2022-05-09 17:51:33 +0100

    first real commit. beginning a data load script for EC analyses

---
+++ load_data.R
@@ -0,0 +1,9 @@
+## Load all datasets 
+
+# Hyytiala ##
+
+# Daily
+hyyt_daily <- read.csv("~/RDM/DATA/hyytiala/ICOS_drought2018data_hyyt/FLX_FI-Hyy_FLUXNET2015_FULLSET_1996-2018_beta-3/FLX_FI-Hyy_FLUXNET2015_FULLSET_DD_1996-2018_beta-3.csv")
+
+
+#Sodankyla
