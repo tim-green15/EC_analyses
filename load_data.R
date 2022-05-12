9b24722325e21f2b4292600953bfbcdcb7f9140a
tim-green15 <tim.green@ed.ac.uk>
2022-05-12 11:31:26 +0100

    q

--- load_data.R
+++ load_data.R
@@ -7,4 +7,28 @@ hyyt_daily <- read.csv("~/RDM/DATA/hyytiala/ICOS_drought2018data_hyyt/FLX_FI-Hyy
 
 
 #Sodankyla
-sod_daily <- read.csv()
+
+# Hourly
+sod_hourly_flux <- read_csv("/exports/csce/datastore/geos/users/s2139773/DATA/sodankyla/LCC_sod_data/LCC_sod_hourly_flux.csv", 
+                                         col_types = cols(`Date (UTC end of averaging period)` = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+
+
+sod_hourly_met <- read_csv("/exports/csce/datastore/geos/users/s2139773/DATA/sodankyla/LCC_sod_data/LCC_sod_hourly_met_rmROW1.csv", 
+                           col_types = cols(SWPa020 = col_double(), 
+                                            SWPb020 = col_double(), STa005 = col_double(), 
+                                            STa010 = col_double(), STa030 = col_double(), 
+                                            STa050 = col_double(), STa100 = col_double(), 
+                                            STb005 = col_double(), STb010 = col_double(), 
+                                            STb020 = col_double(), STb050 = col_double(), 
+                                            STb100 = col_double(), SMb005 = col_double(), 
+                                            SMb010 = col_double(), SMb020 = col_double(), 
+                                            SMb050 = col_double()))
+
+
+# Kentarova
+
+Kenttarova_hourly <- read_csv("/exports/csce/datastore/geos/users/s2139773/DATA/kenttarova/Kenttärova_gapfilled.csv", 
+                                     col_types = cols(Date = col_datetime(format = "%d/%m/%Y %H:%M")))
+
+
+# Convert Sod and Kent hourly -> daily 
